5b962c4c7d3084c620dc050254b3d47066ab37a5
Ulya Ivancova <ulya-rubo@mail.ru>
2021-03-24 14:40:42 +0200

    commite

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int yearOfBirthday = 1990;
+        int monthOfBirthday = 01;
+        int dayOfBirthday = 06;
+        int sum = yearOfBirthday + monthOfBirthday + dayOfBirthday;
+        System.out.println ("Сумма моего года, месяца и дня рождения:"+" " + sum);
+        boolean monthBiggerDay;
+        if (monthOfBirthday>dayOfBirthday)
+            monthBiggerDay = true;
+        else
+            monthBiggerDay = false;
+        System.out.println ("Месяц моего рождения больше даты рождения:"+" " +  monthBiggerDay);
+
+        }	// write your code here
     }
-}
+
